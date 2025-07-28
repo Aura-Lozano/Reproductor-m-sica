42c7782b112617457bb4bb3d64e37601c889eb45
Aura-Lozano <auracristinalozanohoyos@gmail.com>
2025-07-28 17:17:21 -0500

    Primeros avances del reproductor de música

---
+++ codigo/app.js
@@ -0,0 +1,14 @@
+angular.module("Reproductor", [])
+.controller("ControladorReproductor", function ($scope, $http){
+
+    $scope.artistas=[];
+
+    //cargue de datos (solo posible mediante servicio)
+    $http.get("datos/Musiteca.json")
+    .then(function(datos){
+        $scope.artistas = datos.data.Artistas;
+    });
+
+
+});
+
