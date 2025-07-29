d8a94bba5f451b8dbca9cde12654dc630ff7598d
Aura-lozano <auracristinalozanohoyos@gmail.com>
2025-07-29 17:16:50 -0500

    se complemntó el programa

--- codigo/app.js
+++ codigo/app.js
@@ -3,14 +3,44 @@ angular.module("Reproductor", [])
 
     $scope.artistas=[];
     $scope.canciones = [];
+    $scope.artistaSeleccionado = "";
+    $scope.cancionSeleccionada = "";
+    $scope.rutaCancion="";
+    $scope.reproductor = new Audio();
+    $scope.duracion = 0;
+    $scope.tiempoActual =0;
+
     //cargue de datos (solo posible mediante servicio)
     $http.get("datos/Musiteca.json")
-    .then(function(datos){
+        .then(function(datos){
         $scope.artistas = datos.data.Artistas;
     });
 
     $scope.seleccionarArtista = function (artista){
         $scope.canciones = artista.Canciones;
+        $scope.artistaSeleccionado=artista.Nombre;
+    }
+
+    $scope.seleccionarCancion = function (cancion) {
+        $scope.cancionSeleccionada = cancion.Titulo + "  " + $scope.artistaSeleccionado;  
+        $scope.rutaCancion ="canciones/"+$scope.artistaSeleccionado + " - " + cancion.Titulo + ".mp3" ;
+        $scope.reproductor.src = $scope.rutaCancion;
+        $scope.duracion = $scope.reproductor.duration;
+        $scope.tiempoActual = 0;
+    }
+    
+    $scope.reproducir = function () {
+        $scope.reproductor.play();
+    }
+
+    $scope.pausar = function () {
+        $scope.reproductor.pause();
+    }
+
+    $scope.ubicarTiempoCancion = function (){
+        $scope.reproductor.currenTime = $scope.tiempoActual; 
     }
 });
+
+
 
