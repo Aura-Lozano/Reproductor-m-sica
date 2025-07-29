a8d188bb0c96875184b76109251890db1854b8a1
Aura-lozano <auracristinalozanohoyos@gmail.com>
2025-07-29 11:20:05 -0500

    Se agregó la parte de canciones y se complementaron sus respectivos estilos

--- codigo/app.js
+++ codigo/app.js
@@ -2,13 +2,15 @@ angular.module("Reproductor", [])
 .controller("ControladorReproductor", function ($scope, $http){
 
     $scope.artistas=[];
-
+    $scope.canciones = [];
     //cargue de datos (solo posible mediante servicio)
     $http.get("datos/Musiteca.json")
     .then(function(datos){
         $scope.artistas = datos.data.Artistas;
     });
 
-
+    $scope.seleccionarArtista = function (artista){
+        $scope.canciones = artista.Canciones;
+    }
 });
 
